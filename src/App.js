aa3641172c45571da858effae63e587d40ef0fc5
JustinBleile <justinbleile.dev@gmail.com>
2022-04-11 20:01:14 -0600

    boilerplate set up api connections working

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import Button from "./components/CallApi";
 function App() {
-  const callApi = async () => {
-    await fetch("/trade")
-      .then((response) => response.text())
-      .then((data) => console.log(data));
-  };
 
-  return (
-    <div>
-      <Button onClick={callApi}></Button>
-    </div>
-  );
+  return (<div>
+    <Button/>
+  </div>);
 }
 
 export default App;
--- src/components/CallApi.js
+++ src/components/CallApi.js
@@ -1,9 +1,12 @@
 const CallApi = (props) => {
-  return (
-    <button className="callApiButt" onClick={props.onClick}>
-      Call Api
-    </button>
-  );
+
+  const callApi = async () => {
+    await fetch("/trade").then((response) => response.text()).then((data) => console.log(data));
+  };
+
+  return (<button className="callApiButt" onClick={callApi}>
+    Call Api
+  </button>);
 };
 
 export default CallApi;
