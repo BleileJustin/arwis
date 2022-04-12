c4a7e197a461fc85e6e8f8a00b07de3fca9c7654
JustinBleile <justinbleile.dev@gmail.com>
2022-04-12 10:48:47 -0600

    Github push test

--- src/components/CallApi.js
+++ src/components/CallApi.js
@@ -1,12 +1,15 @@
 const CallApi = (props) => {
-
   const callApi = async () => {
-    await fetch("/trade").then((response) => response.text()).then((data) => console.log(data));
+    await fetch("/trade")
+      .then((response) => response.text())
+      .then((data) => console.log(data));
   };
 
-  return (<button className="callApiButt" onClick={callApi}>
-    Call Api
-  </button>);
+  return (
+    <button className="callApiButt" onClick={callApi}>
+      Call Api
+    </button>
+  );
 };
 
 export default CallApi;
