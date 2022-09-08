359dc7ace3a0d413a7e2423640c12e413ff13abd
JustinBleile <justinbleile.dev@gmail.com>
2022-09-07 23:26:02 -0600

    Working delete_bar button

--- src/components/Dashboard/algorithms/Algorithms.js
+++ src/components/Dashboard/algorithms/Algorithms.js
@@ -1,17 +1,53 @@
 import Bar from "./Bar/Bar";
+import { useState } from "react";
 
 import css from "./Algorithms.module.css";
 
 const Algorithms = () => {
+  const [bars, setBars] = useState([
+    {
+      id: 0,
+      curPair: "ETH/USD",
+      price: "$0.00",
+    },
+  ]);
   const addBar = () => {
     console.log("ADD_BAR CLICKED");
   };
+
+  const deleteBarHandler = (barId) => {
+    setBars((prevBars) => {
+      const updatedBars = prevBars.filter((bar) => bar.id !== barId);
+      console.log(updatedBars)
+      return updatedBars;
+    });
+  };
+
   return (
     <div className={css.algorithms}>
-      <Bar></Bar>
-      <button className={css.add_bar} onClick={addBar}/>
+      <ul>
+        {bars.map((bar) => (
+          <Bar key={bar.id} id={bar.id} onDeleteBar={deleteBarHandler}>{bar.data}</Bar>
+        ))}
+      </ul>
+      <button className={css.add_bar} onClick={addBar} />
     </div>
   );
 };
 
 export default Algorithms;
+
+/*
+{
+        id: 0,
+        curPair: 'ETH/USD',
+        price: '$0.00',
+
+      },
+      {
+        id: 1,
+        curPair: 'BTC/USD',
+        price: '$0.00'
+      }
+}
+*/
--- src/components/Dashboard/algorithms/Bar/Bar.js
+++ src/components/Dashboard/algorithms/Bar/Bar.js
@@ -1,15 +1,22 @@
 import css from "./Bar.module.css";
 
-const Bar = () => {
+const Bar = (props) => {
   const expandBar = () => {
     console.log("EXPAND_BAR CLICKED");
   };
+  const id = props.id;
+
+  const deleteBar = () => {
+    props.onDeleteBar(id);
+    console.log("DELETE_BAR CLICKED")
+  }
 
   return (
     <div className={css.bar}>
       <button className={css.expand_bar} onClick={expandBar}></button>
       <h2 className={css.cur_pair}> </h2>
       <h3 className={css.wallet_value}> </h3>
+      <button className={css.delete_bar} onClick={deleteBar}></button>
     </div>
   );
 };
