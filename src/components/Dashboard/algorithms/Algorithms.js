845af6727cc3a02108b7d115c3e79ce4f2fc1750
JustinBleile <justinbleile.dev@gmail.com>
2022-09-14 22:19:12 -0600

    More Algorithm Bar state logic

--- src/components/Dashboard/algorithms/Algorithms.js
+++ src/components/Dashboard/algorithms/Algorithms.js
@@ -1,58 +1,41 @@
 import Bar from "./Bar/Bar";
-import { useState } from "react";
+import React, { useState } from "react";
 
 import css from "./Algorithms.module.css";
 
 const Algorithms = () => {
-  const [bars, setBars] = useState([
-    {
-      id: 0,
-      curPair: "ETH/USD",
-      price: "$0.00",
-    },
-  ]);
-  const addBar = () => {
+  const [algorithms, setAlgorithms] = useState([]);
+
+  const addBarHandler = () => {
     console.log("ADD_BAR CLICKED");
-    //(DESIGN)!!!OPEN FORM INSIDE NEW BAR IN DOM!!!
-  };
-
-  const connectWallet = () => {
-    console.log('WALLET CONNECTED')
-  }
-
-  const deleteBarHandler = (barId) => {
-    setBars((prevBars) => {
-      const updatedBars = prevBars.filter((bar) => bar.id !== barId);
-      console.log(updatedBars)
-      return updatedBars;
+    setAlgorithms((prevBars) => {
+      prevBars.push({ id: prevBars.length });
+      console.log(...prevBars);
+      const prev = [...prevBars];
+      return prev;
     });
   };
 
+  const deleteBarHandler = (barId) => {
+    setAlgorithms((prevBars) => {
+      const updatedBars = prevBars.filter((bar) => bar.id !== barId);
+      console.log(updatedBars);
+      return updatedBars;
+    });
+  };
+  const content = algorithms.map((bar) => (
+    <Bar key={bar.id} id={bar.id} onDeleteBar={deleteBarHandler}></Bar>
+  ));
+  console.log(content);
+
   return (
-    <div className={css.algorithms}>
-      <ul>
-        {bars.map((bar) => (
-          <Bar key={bar.id} id={bar.id} onDeleteBar={deleteBarHandler}>{bar.data}</Bar>
-        ))}
-      </ul>
-      <button className={css.add_bar} onClick={addBar} />
-    </div>
+    <React.StrictMode>
+      <div className={css.algorithms}>
+        <ul>{content}</ul>
+        <button className={css.add_bar} onClick={addBarHandler} />
+      </div>
+    </React.StrictMode>
   );
 };
 
 export default Algorithms;
-
-/*
-{
-        id: 0,
-        curPair: 'ETH/USD',
-        price: '$0.00',
-
-      },
-      {
-        id: 1,
-        curPair: 'BTC/USD',
-        price: '$0.00'
-      }
-}
-*/
--- src/components/Dashboard/algorithms/Bar/Bar.js
+++ src/components/Dashboard/algorithms/Bar/Bar.js
@@ -1,22 +1,37 @@
+import { useState } from "react";
+
 import css from "./Bar.module.css";
+import BarForm from "./BarForm/BarForm";
 
 const Bar = (props) => {
+  const [barState, setBarState] = useState();
   const expandBar = () => {
     console.log("EXPAND_BAR CLICKED");
   };
+
   const id = props.id;
 
   const deleteBar = () => {
     props.onDeleteBar(id);
-    console.log("DELETE_BAR CLICKED")
-  }
+    console.log("DELETE_BAR CLICKED");
+  };
 
-  return (
+  const onConnect = (curPair) => {
+    setBarState(
+      <div>
+        <button className={css.expand_bar} onClick={expandBar}></button>
+        <h2 className={css.cur_pair}>{curPair}</h2>
+        <h3 className={css.wallet_value}> </h3>
+        <button className={css.delete_bar} onClick={deleteBar}></button>
+      </div>
+    );
+  };
+
+  return barState ? (
+    <div className={css.bar}>{barState}</div>
+  ) : (
     <div className={css.bar}>
-      <button className={css.expand_bar} onClick={expandBar}></button>
-      <h2 className={css.cur_pair}> </h2>
-      <h3 className={css.wallet_value}> </h3>
-      <button className={css.delete_bar} onClick={deleteBar}></button>
+      <BarForm onConnect={onConnect}></BarForm>
     </div>
   );
 };
