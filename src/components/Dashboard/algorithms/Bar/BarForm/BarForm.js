6d9d5d5e4ec9b157a9a15b7d496702d83d901685
JustinBleile <justinbleile.dev@gmail.com>
2022-09-14 22:17:52 -0600

    More Algorithm Bar state logic

---
+++ src/components/Dashboard/algorithms/Bar/BarForm/BarForm.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+import css from "./BarForm.module.css";
+
+const BarForm = (props) => {
+  const [dropdownState, setDropdownState] = useState("");
+
+  const connect = (event) => {
+    event.preventDefault();
+    console.log(dropdownState);
+    props.onConnect(dropdownState);
+  };
+  return (
+    //Eventually eliminate hard coded options
+    <form className={css.wallet_form} onSubmit={connect} id="wallet_form">
+      <select
+        className={css.cur_pair}
+        form="wallet_form"
+        onChange={(e) => setDropdownState(e.target.value)}
+      >
+        <option value="select">Pair</option>
+        <option value="BTC/USD">BTC/USD</option>
+        <option value="ETH/USD">ETH/USD</option>
+      </select>
+      <input type="submit" value="Connect" className={css.connect}/>
+    </form>
+  );
+};
+
+export default BarForm;
