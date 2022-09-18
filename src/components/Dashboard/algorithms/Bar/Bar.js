cb29bca4e11f8f3fb1fd5eb6127f064a0d8af534
JustinBleile <justinbleile.dev@gmail.com>
2022-09-17 23:08:21 -0600

    More Bar and BarForm styling

--- src/components/Dashboard/algorithms/Bar/Bar.js
+++ src/components/Dashboard/algorithms/Bar/Bar.js
@@ -20,10 +20,14 @@ const Bar = (props) => {
     curPair
       ? setBarState(
           <div className={css.bar}>
-            <button className={css.expand_bar} onClick={expandBar}></button>
-            <h2 className={css.cur_pair}>{curPair}</h2>
-            <h3 className={css.wallet_value}> </h3>
-            <button className={css.delete_bar} onClick={deleteBar}></button>
+            <div className={css.cur_pair_container}>
+              <button className={css.expand_bar} onClick={expandBar}></button>
+              <h2 className={css.cur_pair}>{curPair}</h2>
+            </div>
+            <div className={css.wallet_value_container}>
+              <h3 className={css.wallet_value}>Wallet Value:</h3>
+              <button className={css.delete_bar} onClick={deleteBar}></button>
+            </div>
           </div>
         )
       : alert("Validation: Please choose a Pair before connecting");
