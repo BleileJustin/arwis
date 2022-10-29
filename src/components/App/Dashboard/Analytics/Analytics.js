182598a3413726c3e2dd687e2269096a8d0ccb0b
JustinBleile <justinbleile.dev@gmail.com>
2022-10-29 10:10:08 -0600

    Styling changes

--- src/components/App/Dashboard/Analytics/Analytics.js
+++ src/components/App/Dashboard/Analytics/Analytics.js
@@ -2,14 +2,16 @@ import Portfolio from "./Portfolio/Portfolio";
 import TradeList from "./TradeList/TradeList";
 import Distribution from "./Distribution/Distribution";
 
-import css from './Analytics.module.css'
+import css from "./Analytics.module.css";
 
 const Analytics = () => {
   return (
     <div className={css.analytics}>
       <Portfolio></Portfolio>
-      <TradeList></TradeList>
-      <Distribution></Distribution>
+      <div className={css.analytics_right_half_container}>
+        <TradeList></TradeList>
+        <Distribution></Distribution>
+      </div>
     </div>
   );
 };
--- src/components/App/Dashboard/Wallets/Bar/BarExpanded/BarExpanded.js
+++ src/components/App/Dashboard/Wallets/Bar/BarExpanded/BarExpanded.js
@@ -4,6 +4,17 @@ import WalletGraph from "./WalletGraph/WalletGraph";
 import Algorithms from "./Algorithms/Algorithms";
 
 const BarExpanded = (props) => {
+  const tickHandler = async () => {
+    const symbol = prompt("Choose a currency pair");
+    symbol
+      ? await fetch(`http://localhost:3000/ticker/${symbol}`)
+          .then((res) => res.json())
+          .then((data) => console.log(data))
+      : console.log("err");
+    return symbol;
+  };
+  //tickHandler();
+  
   return (
     <div className={props.css ? css.bar_expanded : css.bar_closed}>
       {props.css ? (
