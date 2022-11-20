ef0697f9b85ba940333abf6a4369e499572536cd
JustinBleile <justinbleile.dev@gmail.com>
2022-11-19 21:05:46 -0700

    Updated gitignore

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,19 @@
-/* eslint-disable object-curly-spacing */
 const express = require("express");
 const app = express();
 const functions = require("firebase-functions");
 const cors = require("cors");
 
-app.use(cors({ origin: "https://arwis1.web.app" }));
+var whitelist = ["http://localhost:3000", "https://arwis1.web.app"];
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS" + origin));
+    }
+  },
+};
+app.use(cors(corsOptions));
 
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
@@ -17,28 +26,23 @@ initializeApp({
 
 const db = getFirestore();
 
+const port = 80;
+
 // SERVER-DEV
 
-app.use("/write/:userid", (req, res) => {
-  const demo = {
-    first: req.params.userid,
-    last: "Demo",
-    curPairs: {
-      c1: "BTCUSD",
-      c2: "ETHUSD",
-    },
-  };
+//// WRITE
+app.use("/write/:userid", async (req, res) => {
+  let count = 0;
 
   const snapshot = db.collection("users").doc(req.params.userid);
 
-  snapshot.set(demo);
-  const routeResponse = {
-    messaage: "SUCCESSFUL WRITE TO DATABASE",
-    data: demo,
-  };
-  res.send(routeResponse);
+  snapshot.set({ count: count }, { merge: true });
+  const doc = await snapshot.get();
+  const json = JSON.stringify(doc.data());
+  res.send(json);
 });
 
+//// READ
 app.use("/read/:userid", async (req, res) => {
   const snapshot = db.collection("users").doc(req.params.userid);
   const doc = await snapshot.get();
@@ -48,17 +52,32 @@ app.use("/read/:userid", async (req, res) => {
   // res.send("hello");
 });
 
-app.use("/instance", (req, res) => {
-  res.send("Instance Started");
-});
-
 app.use("/count", (req, res) => {
-  res.send();
-});
-// Root Render
-
-app.use("/", (req, res) => {
-  res.send();
+  res.send("count");
 });
 
+//////////////////////////////////////////////////////
+// INSTANCE
+
+const startInstance = async (uid) => {
+  const snapshot = db.collection("users").doc(uid);
+  setInterval(async () => {
+    let countDoc = await snapshot.get();
+    let count = JSON.stringify(countDoc.data().count);
+    count++;
+    snapshot.set({ count: count }, { merge: true });
+  }, 5000);
+};
+
+app.use("/instance/:userid", async (req, res) => {
+  let uid = req.params.userid;
+  const instance = startInstance(uid);
+  res.send(`INSTANCE CREATED FOR ${uid}`);
+});
+
+//////////////////////////////////////////////////////
 exports.app = functions.https.onRequest(app);
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
